d016b266b44a89b440f4cd414b3ccf92bdadc556
NSQA <nguyensyquanganh@gmail.com>
2020-09-23 11:05:20 +0700

    make it better

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const admin = require("firebase-admin");
+const serviceAccount = require("./key/my-icetea-firebase-adminsdk-cyitm-6a7523f508.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://my-icetea.firebaseio.com"
+});
+
+const port = 3000;
+
+
+app.use(bodyParser.json());
+
+const notification_options = {
+    priority: "high",
+    timeToLive: 60 * 60 * 24
+};
+
+const message = {
+    data: {
+        type: "hello",
+        infoMessenger: "test",
+    },
+    notification: {
+        title: "hello",
+        body: "test",
+    }
+};
+
+app.post('/firebase/noti', (req, res) => {
+
+    
+    const  registrationToken = req.body.registrationToken
+    const options =  notification_options
+    
+      admin.messaging().sendToDevice(registrationToken, message, options)
+      .then( res => {
+
+       console.log(res);
+       
+      })
+      .catch( error => {
+          console.log(error);
+      });
+});
+
+app.listen(port, () => console.log('Server is listening...'));
+
